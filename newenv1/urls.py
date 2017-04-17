addb5d9d7409a257a901530eef6d42ec300c9216
Yan Winner <yan.hadzhyisky@gmail.com>
2017-04-17 19:00:27 +0000

    add new url masks

--- newenv1/urls.py
+++ newenv1/urls.py
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
+from students import views as myapp_views
 
 urlpatterns = [
+    #students url
+    url(r'^$', myapp_views.students_list, name='home'),
+    url(r'^students/add/$', myapp_views.students_add, name='students_add'),
+    url(r'^students/(?P<sid>\d+)/edit/$', myapp_views.students_edit, name='students_edit'),
+    url(r'^students/(?P<sid>\d+)/delete/$', myapp_views.students_delete, name='students_delete'),
+    #groups url
+    url(r'^groups/$', myapp_views.groups_list, name='groups'),
+    url(r'^groups/add/$', myapp_views.groups_add, name='groups_add'),
+    url(r'^groups/(?P<gid>\d+)/edit/$', myapp_views.groups_edit, name='groups_edit'),
+    url(r'^groups/(?P<gid>\d+)/delete/$', myapp_views.groups_delete, name='groups_delete'),
     url(r'^admin/', admin.site.urls),
 ]
--- students/views.py
+++ students/views.py
@@ -1,6 +1,22 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#view for Students
+def students_list(request):
+    return render(request, 'students/students_list.html', {})
+def students_add(request):
+    return HttpResponse('<h1>Students Add Form</h1>')
+def students_edit(request, sid):
+    return HttpResponse('<h1>Students Edit %s Form</h1>' % sid)
+def students_delete(request, sid):
+    return HttpResponse('<h1>Students Delete %s Form</h1>' % sid)
+
+#view for Groups
+def groups_list(request):
+    return HttpResponse('<h1>Groups List Form</h1>')
+def groups_add(request):
+    return HttpResponse('<h1>Groups Add Form</h1>')
+def groups_edit(request, gid):
+    return HttpResponse('<h1>Groups Edit %s Form</h1>' % gid)
+def groups_delete(request, gid):
+    return HttpResponse('<h1>Groups Delete %s Form</h1>' % gid)
